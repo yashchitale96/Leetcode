6d871c76749e9ad556d82e1987aedc59029cf6e2
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2023-09-19 21:41:59 +0530

    Time: 5 ms (16.65%), Space: 8.7 MB (5.13%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> exp(heights);
+        sort(exp.begin(),exp.end());
+        int cnt = 0;
+        for(int i=0; i<heights.size();i++)
+        {
+            if(heights[i] != exp[i])
+            {
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+};
