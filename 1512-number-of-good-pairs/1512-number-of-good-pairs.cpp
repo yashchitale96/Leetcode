a1c045cacfe5276e3c9aa43b9745215770997b0c
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2023-10-03 19:21:24 +0530

    Time: 4 ms (15.06%), Space: 7.5 MB (27.67%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int mem[101] = {0};
         int sum = 0;
-
         for(int i=0; i<nums.size(); i++)
         {
-            sum += mem[nums[i]];
-            mem[nums[i]]++;
+            for(int j=i+1; j<nums.size();j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    sum++;
+                }
+            }
         }
-
+        
         return sum;
     }
 };
