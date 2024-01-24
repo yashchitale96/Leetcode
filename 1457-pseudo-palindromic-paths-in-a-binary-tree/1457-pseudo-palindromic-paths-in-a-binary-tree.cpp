797a60e882a35d2c9a305dcc14c07f23ad0df81b
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2024-01-24 14:44:49 +0530

    Time: 221 ms (98.39%), Space: 178.3 MB (96.51%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void solve(TreeNode* root, unordered_map<int, int>& count, int& pathCount) {
+        if (root == nullptr) return;
+
+        count[root->val]++;
+
+        // If it's a leaf node, check for pseudo-palindrome
+        if (root->left == nullptr && root->right == nullptr) {
+            int oddCount = 0;
+            for (auto it : count) {
+                if (it.second % 2 != 0) {
+                    oddCount++;
+                }
+            }
+            if (oddCount <= 1) {
+                pathCount++;
+            }
+        }
+
+        solve(root->left, count, pathCount);
+        solve(root->right, count, pathCount);
+
+        // Backtrack: remove the current node from the count
+        count[root->val]--;
+    }
+
+    int pseudoPalindromicPaths(TreeNode* root) {
+        unordered_map<int, int> count;
+        int pathCount = 0;
+
+        solve(root, count, pathCount);
+
+        return pathCount;
+    }
+};
